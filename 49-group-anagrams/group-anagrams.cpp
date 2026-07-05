60d727e00e92d8d1eff01c55e91d9ec485bc75d3
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-05 16:51:14 +0530

    Time: 12 ms (83.97%) | Memory: 24.9 MB (87.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,42 +1,70 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         vector<string> temp  = strs;
+//        for(int i = 0 ; i< temp.size() ;i++){
+//         sort(temp[i].begin() , temp[i].end());
+//        }
+
+//     //    unordered_map <string , vector > mp ;
+
+//     //    for(auto ele : temp ){
+//     //     if(mp.find(ele) != end()){
+//     //         mp[ele] =
+
+//     //     }
+
+//     //    }
+
+//      vector<vector<string>> ans ;
+//      unordered_set<string> mp ;
+//     for(int i = 0 ;i < temp.size() ;i++){
+//         if(mp.find(temp[i])  != mp.end()) continue ;
+//         mp.insert(temp[i]) ;
+//         vector<string> store ;
+//         string ele = temp[i];
+//         store.push_back(strs[i]);
+//         for(int j = i+1 ; j < temp.size() ;j++){
+//             if(temp[j] == ele ){
+//                 store.push_back(strs[j]) ;
+//             }
+
+//         }
+//         ans.push_back(store);
+//     }
+
+//         return ans ;
+
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> temp  = strs;
-       for(int i = 0 ; i< temp.size() ;i++){
-        sort(temp[i].begin() , temp[i].end());
-       }
+        // Maps the sorted version of a string to all its original anagrams
+        unordered_map<string, vector<string>> mp;
 
-    //    unordered_map <string , vector > mp ;
-
-    //    for(auto ele : temp ){
-    //     if(mp.find(ele) != end()){
-    //         mp[ele] = 
-
-    //     }
-        
-    //    }
-
-     vector<vector<string>> ans ; 
-     unordered_set<string> mp ;
-    for(int i = 0 ;i < temp.size() ;i++){
-        if(mp.find(temp[i])  != mp.end()) continue ;
-        mp.insert(temp[i]) ;
-        vector<string> store ;
-        string ele = temp[i];
-        store.push_back(strs[i]);
-        for(int j = i+1 ; j < temp.size() ;j++){
-            if(temp[j] == ele ){
-                store.push_back(strs[j]) ;
-            }
-
+        for (const string& s : strs) {
+            string sorted_s = s;
+            sort(sorted_s.begin(), sorted_s.end()); // O(K log K)
+            mp[sorted_s].push_back(s);              // O(K) hash map insertion
         }
-        ans.push_back(store);
-    }
+ 
+        // Collect results from the
 
+        // for (const auto& [key, anagrams] : mp) {
+        //     cout << "Key (Sorted): " << key << " -> Anagrams: ";
+        //     for (const string& str : anagrams) {
+        //         cout << str << " ";
+        //     }
+        //     cout << endl;
+        // }
 
-        return ans ;
-       
-        
-        
+        vector<vector<string>> ans;
+        for (auto& pair : mp) {
+            ans.push_back(pair.second);
+        }
+
+        return ans;
     }
 };
