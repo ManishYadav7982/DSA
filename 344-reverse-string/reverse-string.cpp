e2646b5aac26f22aceffa29e313bda2e8e352d29
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-06 02:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (80.78%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0 ;
+        int j = s.size() -1 ;
+        // cout << s[j] ; 
+        while(i <= j ){
+            swap(s[i] , s[j]);
+            i++ , j-- ;
+        }
+        
+    }
+};
