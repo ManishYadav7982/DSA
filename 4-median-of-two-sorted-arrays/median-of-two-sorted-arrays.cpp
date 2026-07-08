f941b70db6bd8683e05eef63238c41274da942f5
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-08 21:09:49 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.86%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,53 +1,38 @@
 class Solution {
-    private :
-
-    void print(vector<int > &v ){
-        for(int i : v){
-            cout << i << " ";
-        }
-    }
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int > temp ;
-        int i = 0 ;
-        int j = 0 ;
+        if (nums2.size() < nums1.size())
+            return findMedianSortedArrays(nums2, nums1);
 
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] < nums2[j]){
-                temp.push_back(nums1[i]);
-                i++ ;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int low = 0, high = n1;
+
+        while (low <= high) {
+            int cut1 = low + (high - low) /2 ;
+            int cut2 = (n1 + n2 + 1) / 2- cut1 ;
+
+
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
+
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((n1 + n2) % 2 == 0) {
+                    return (max(left1, left2) + min(right1, right2)) / 2.0;
+                } else
+                    return max(left1, left2);
             }
-            else {
-                temp.push_back(nums2[j]);
-                j++ ;
+
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
             }
         }
 
-        while(i < nums1.size()){
-            temp.push_back(nums1[i]);
-            i++ ;
-        }
-        while(j < nums2.size() ){
-            temp.push_back(nums2[j]);
-            j++ ; 
-        }
-
-        print(temp );
-
-        int n = temp.size();
-        // cout << " n " << n ; 
-
-        double  val  ;
-        int ind = n/2 ;
-        if(n %2 == 0 ){
-            val = temp[ind] + temp[ind -1 ];
-             return val/2.00000 ;
-        }
-        else{
-          val = temp[ind];
-          return val ;
-
-        }
-
+        return 0.0;
     }
 };
