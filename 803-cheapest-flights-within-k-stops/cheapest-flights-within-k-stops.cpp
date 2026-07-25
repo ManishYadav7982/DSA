6049a48ffc6b258cf86fd661b40374a7235acc34
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-26 02:42:05 +0530

    Time: 4 ms (44.81%) | Memory: 18.5 MB (31.23%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,45 @@
-////
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
         vector<vector<pair<int, int>>> adj(n);
-        for ( auto& flight : flights) {
-            adj[flight[0]].push_back({flight[1], flight[2]});
+
+        for (auto e : flights) {
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
+            adj[u].push_back({v, wt});
         }
 
-        vector<int> minCost(n, 1e9);
-        minCost[src] = 0;
+        // for(int i =0;i<n;i++){
+        //     cout << i << "-> " ;
+        //     for(auto ele : adj[i]){
+        //         cout << ele.first << "\t" << ele.second ;
+        //     }
+        //     cout << endl ;
+        // }
 
-        queue<pair<int, int>> q;
-        q.push({src, 0});
+        queue<tuple<int, int, int>> q; // K , node , wt
+        q.push({0, src, 0});
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
 
-        int stops = 0;
-        while (!q.empty() && stops <= k) {
-            int size = q.size();
-            
-            while (size--) {
-                auto [node, cost] = q.front();
-                q.pop();
+        while (!q.empty()) {
+            auto [stops, curr, wt] = q.front();
+            q.pop();
+            if (stops > k)
+                continue;
+            for (auto [nbd, edgw] : adj[curr]) {
 
-                for (auto& [neighbor, price] : adj[node]) {
-                    if (cost + price < minCost[neighbor]) {
-                        minCost[neighbor] = cost + price;
-                        q.push({neighbor, minCost[neighbor]});
-                    }
+                if (wt + edgw < dist[nbd] && stops <= k) {
+                    dist[nbd] = wt + edgw;
+                    q.push({stops + 1, nbd, wt + edgw});
                 }
             }
-            stops++;
         }
 
-        return minCost[dst] == 1e9 ? -1 : minCost[dst];
+        if (dist[dst] == 1e9)
+            return -1;
+        return dist[dst];
     }
 };
