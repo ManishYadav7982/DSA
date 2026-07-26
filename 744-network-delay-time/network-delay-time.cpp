b76c5853af96cbf046d35182d3e53f52f8b117d3
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-26 15:31:06 +0530

    Time: 84 ms (53.53%) | Memory: 49.1 MB (5.66%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        vector<vector<pair<int, int>>> adj(n+1 );
+        for (auto e : times) {
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
+
+            adj[u].push_back({wt, v}) ;// src  = {wt , dest} ;
+        }
+
+        priority_queue<tuple<int, int, int>,   
+                        vector<tuple<int, int, int>>,
+                        greater<tuple<int, int, int>>>q ;  // time , startingnode ,targetnode 
+
+        vector<int> dist(n +1 , 1e9) ;
+        dist[0] = -1 ;
+
+        q.push({0,k,k}) ;
+        dist[k] = 0 ;
+
+        while(!q.empty()){
+            auto [currwt , src , dst] = q.top() ;
+            q.pop() ;
+
+            if(dist[dst] < currwt) continue ;
+
+            for(auto ele : adj[dst]){ // travel on nbd of dst node 
+                auto [nbd_wt , nbd] = ele ;
+                if(nbd_wt + currwt < dist[nbd]){
+                    dist[nbd] = nbd_wt + currwt ;
+                    q.push({nbd_wt + currwt , src , nbd}) ;
+                }
+
+
+            }
+
+        }
+
+        for(int e : dist ){
+            // cout << e << ' ' ;
+            if( e == 1e9) return -1 ;
+        }
+        return *max_element(dist.begin() , dist.end()) ;
+
+
+          
+    }
+};
