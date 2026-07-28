9cd785599c67c8a48dc2a93f4fbcb84329764649
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-28 12:25:16 +0530

    Time: 19 ms (47.94%) | Memory: 45.8 MB (71.66%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        sort(people.begin() , people.end());
+
+        int l=0 ;
+        int h = n-1 ; // 
+        int cnt =0 ;
+        while(l <h){
+            if(people[l] + people[h] <= limit  ){
+                cnt++ ;
+                l++ ;
+                h--;
+            }
+           else cnt++ , h-- ;
+        //    cout << cnt << "H : " << h << "L : " << l << ' ' ;
+        }
+        if(l==h){
+            cnt++ ;
+        }
+        return cnt ;
+
+
+
+    }
+};
