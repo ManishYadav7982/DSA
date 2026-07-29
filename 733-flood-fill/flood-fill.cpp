f255961047a5562e75cb4fb214bf66a38b35c20e
Manish Yadav DTU <manishya12390@gmail.com>
2026-07-29 14:15:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (31.34%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int>> vis = image;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size() ;
+        int m = image[0].size() ;
+         if(image[sr][sc] == color)
+            return image;
 
-        queue<pair<int, int>> q;
-        int target = image[sr][sc];
-        q.push({sr, sc});
-        vis[sr][sc] = color;
+        vector<vector<int>> vis = image ;
 
-        while (q.size() > 0) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            int rw[] = {0, -1, 0, 1};
-            int cl[] = {-1, 0, 1, 0};
-            for (int i = 0; i < 4; i++) {
-                int row = r + rw[i];
-                int col = c + cl[i];
-                if (row >= 0 && row < n && col >= 0 && col < m &&
-                    image[row][col] == target && vis[row][col] != color) {
-                    vis[row][col] = color;
-                    q.push({row, col});
+        queue<pair<int,int>> q ;
+        q.push({sr , sc }) ;
+        int isInialColor = image[sr][sc] ;
+        vis[sr][sc] = color ;
+
+        while(!q.empty()){
+            auto [rw , cl ] = q.front() ;
+            q.pop() ;
+
+            int row[] = {0 , -1 , 0 , 1} ;
+            int col[] = {-1 ,0 , 1 , 0 } ;
+
+            for(int i=0;i<4;i++){
+                int n_row = rw + row[i] ;
+                int n_col = cl + col[i] ;
+
+                if(n_row >= 0 && n_row < n && n_col >=0 && n_col < m && vis[n_row][n_col] ==isInialColor){
+                    vis[n_row][n_col] = color ;
+                    q.push({n_row , n_col }) ;
                 }
             }
+
         }
 
-        return vis;
+        return vis ;
+
+        
     }
 };
