11ac9ad561b8c8c635b9524e53a38f6e256b0b0f
Manish Yadav DTU <manishya12390@gmail.com>
2026-08-04 19:29:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.25%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
+    private:
+    double findpow(double x , long long n ){
+        if(n == 0 ) return 1;
+
+        double half =findpow(x,n/2) ;
+        double res = half*half ;
+
+        if(n%2 == 1){
+            res = x*res ;
+        }
+
+        return res ; 
+    }
+
 public:
     double myPow(double x, int n) {
-        return pow(x , n ) ; 
+        long long N= n ; 
+       if(n < 0 ) x = 1/x , N = -N; 
+       return findpow(x , N ) ; 
+
     }
 };
