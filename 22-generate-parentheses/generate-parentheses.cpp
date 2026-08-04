c2dabe4ca6aa8de78b48768eb7ca578976c6e578
Manish Yadav DTU <manishya12390@gmail.com>
2026-08-04 22:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (95.85%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,43 +1,45 @@
 class Solution {
-    private:
+private:
+    // bool isValid(string& curr) {
+    //     int cnt = 0;
+    //     for (auto i : curr) {
+    //         if (i == '(')
+    //             cnt++;
+    //         else
+    //             cnt--;
 
-    bool isValid(string &curr){
-        int cnt = 0;
-        for(auto i : curr){
-            if(i=='(') cnt++ ;
-            else cnt-- ;
+    //         if (cnt < 0)
+    //             return false;
+    //     }
 
-            if(cnt < 0 ) return false ;
+    //     return cnt == 0;
+    // }
+
+    void made(vector<string>& ans, string& curr, int n, int open, int close) {
+        if ((int)curr.size() == 2 * n) {
+            ans.push_back(curr);
+            return;
         }
 
-        return cnt ==  0 ;
-    }
-    
-    void made( vector<string>&ans , string &curr , int n ){
-        if((int)curr.size() == 2*n){
-            if(isValid(curr)){
-                ans.push_back(curr);
-            }
-
-            return ;
-        }
-
-            // backtracking 
-
-            curr.push_back(')');
-            made(ans , curr ,n );
-            curr.pop_back();
-
+        // backtracking
+        // do someting then expore then again apply updo
+        if (open < n) {
             curr.push_back('(');
-            made(ans , curr , n );
+            made(ans, curr, n, open + 1, close);
             curr.pop_back();
-        
+        }
+        if (close < open) {
+            curr.push_back(')');
+            made(ans, curr, n, open, close +1);
+            curr.pop_back();
+        }
     }
+
 public:
     vector<string> generateParenthesis(int n) {
-        string curr = "" ;
-        vector<string>ans ;
-        made(ans , curr , n );
-        return ans ; 
+        string curr = "";
+        vector<string> ans;
+        made(ans, curr, n, 0, 0);
+        return ans;
     }
 };
