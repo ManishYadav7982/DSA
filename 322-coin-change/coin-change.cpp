95dfa2ae94560d88e4f515a882396b8b369c548a
Manish Yadav DTU <manishya12390@gmail.com>
2026-08-18 21:58:30 +0530

    Time: 46 ms (37.38%) | Memory: 49 MB (34.82%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,31 +1,49 @@
 class Solution {
-    private :
-    int f(int ind , int amt ,vector<int>& coins, vector<vector<int>> & dp ){
-        if(ind == 0 ){
+private:
+    int f(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp) {
+        if (ind == 0) {
             if (amt % coins[0] == 0)
                 return amt / coins[0];
-            else return 1e9 ;
+            else
+                return 1e9;
         }
-        if(dp[ind][amt] != -1) return dp[ind][amt] ;
+        if (dp[ind][amt] != -1)
+            return dp[ind][amt];
 
-        int nottake = f(ind-1 , amt,coins ,dp);
-        int take = INT_MAX ;
-        if(amt >= coins[ind]){
-           take = 1 + f(ind , amt - coins[ind] , coins ,dp );
-        } 
+        int nottake = f(ind - 1, amt, coins, dp);
+        int take = INT_MAX;
+        if (amt >= coins[ind]) {
+            take = 1 + f(ind, amt - coins[ind], coins, dp);
+        }
 
-        return dp[ind][amt] = min(take , nottake ) ;
+        return dp[ind][amt] = min(take, nottake);
     }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size() ;
-        vector<vector<int>> dp(n  ,vector<int> (amount +1 , -1 )) ;
-        int ans = f(n-1 , amount ,coins,dp ) ;
-        if (ans >= 1e9) {
-            return -1;
-        } else {
-            return ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        for (int amt = 0; amt <= amount; amt++) {
+            if (amt % coins[0] == 0)
+                dp[0][amt] = amt / coins[0];
+            else
+                dp[0][amt] = 1e9;
         }
 
+        for (int i = 1; i < n; i++) {
+            for (int amt = 0; amt <= amount; amt++) {
+
+                int nottake = dp[i-1][amt] ;
+                int take = 1e9;
+                if (amt >= coins[i]) {
+                    take = 1 + dp[i][amt - coins[i]] ;
+                }
+
+                 dp[i][amt] = min(take, nottake) ;
+            }
+        }
+
+        if(dp[n-1][amount] >= 1e9) return -1 ;
+        else return dp[n-1][amount] ;
     }
 };
